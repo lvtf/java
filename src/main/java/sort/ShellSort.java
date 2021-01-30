6a3075f0466b9add1becf782617d5869874cd8c1
tengfeilv <tengfeilv@58btc.com>
2021-01-31 01:35:58 +0800

    xier sort

---
+++ src/main/java/sort/ShellSort.java
@@ -0,0 +1,49 @@
+package sort;
+
+import java.util.Arrays;
+
+public class ShellSort {
+    public static void main(String[] args) {
+        int[] arr = {3, 1, 10, 5};
+        shellSort2(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+
+    //位移法
+    public static void shellSort2(int[] arr) {
+        for (int gap = arr.length / 2; gap > 0; gap /= 2) {
+            for (int i = gap; i < arr.length; i++) {
+                int j = i;
+                int temp = arr[j];
+                if (temp < arr[j - gap]) {
+                    while (j - gap >= 0 && arr[j] < arr[j - gap]) {
+                        arr[j] = arr[j - gap];
+                        j-= gap;
+                    }
+                    arr[j] = temp;
+                }
+
+            }
+        }
+    }
+
+    //交换法
+    public static void shellSort(int[] arr) {
+        int temp = 0;
+        for (int gap = arr.length / 2; gap > 0; gap /= 2) {
+            for (int i = gap; i < arr.length; i++) {
+                for (int j = i - gap; j >= 0; j -= gap) {
+                    if (arr[j] > arr[j + gap]) {
+                        temp = arr[j];
+                        arr[j] = arr[j + gap];
+                        arr[j + gap] = temp;
+                    }
+
+                }
+
+            }
+
+        }
+    }
+}
